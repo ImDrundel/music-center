e84f6a9618e0801ef4916d7d3da992b34a3c8f6e
Andrew <andcher53@gmail.com>
2024-03-04 20:18:42 +0200

    refactor: customise 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,12 @@
-import Image from "next/image"
+import "./globals.scss";
  
 export default function Page404() {
     return (
-    <>
-    <div>Oh no! 404! </div>
-    <div>Oops page not found ^_^</div>
-    {/* <Image></Image> */}
-    </>
+    <div className="error404">
+         <div className="error404__textbox">
+          <div className="erroe404__upLine">Oh no! 404! </div>
+          <div className="erroe404__downLine">Oops page not found ^_^</div>
+         </div>
+    </div>
     )
 }
