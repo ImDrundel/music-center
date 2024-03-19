4edd204e23be752b3e5547e949d41b556fc6b350
ImDrundel <you@example.comandcher53@gmail.com>
2024-03-19 23:05:51 +0200

    feat: add search input

--- app/components/main/oneSong/oneSong.tsx
+++ app/components/main/oneSong/oneSong.tsx
@@ -10,59 +10,54 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { Song } from "./oneSongContainer";
 
-
-
-
-
-export default function OneSong(props: {song: Song}) {
-
-  const [like, setLike] = useState({ isLike: false });
+export default function OneSong(props: { song: Song }) {
+  const [like, setLike] = useState({ isLike: true });
 
   const onClick = () => {
     setLike({ ...like, isLike: !like.isLike });
   };
 
   return (
-          <div className={style.block} key={props.song.id}>
-            <div className={style.image}>
-              <Image src={SongIcon} alt="SongIcon" />
+    <div className={style.block} key={props.song.id}>
+      <div className={style.image}>
+        <Image src={SongIcon} alt="SongIcon" />
+      </div>
+      <div className={style.fullInfo}>
+        <div className={style.nameAndRating}>
+          <div className={style.name}>
+            <Link href={`/song/${props.song.id}`}>{props.song.name}</Link>
+          </div>
+          <div className={style.rating}>Rating: {props.song.rating}</div>
+        </div>
+        <div className={style.infoAndIcons}>
+          <div className={style.smallInfo}>
+            <div className={style.artistAndDuration}>
+              <div className={style.artist}>Artist: {props.song.artist}</div>
+              <div className={style.duration}> {props.song.duration}</div>
             </div>
-            <div className={style.fullInfo}>
-              <div className={style.nameAndRating}>
-                <div className={style.name}>
-                  <Link href={`/song/${props.song.id}`}>{props.song.name}</Link>
-                </div>
-                <div className={style.rating}>Rating: {props.song.rating}</div>
-              </div>
-              <div className={style.infoAndIcons}>
-                <div className={style.smallInfo}>
-                  <div className={style.artistAndDuration}>
-                    <div className={style.artist}>Artist: {props.song.artist}</div>
-                    <div className={style.duration}> {props.song.duration}</div>
-                  </div>
-                  <div className={style.albumAndDate}>
-                    <div className={style.album}>Album: {props.song.album}</div>
-                    <div className={style.date}>{props.song.date}</div>
-                  </div>
-                </div>
-                <div className={style.subbuttons}>
-                  <button className={style.play}>
-                    <Image src={PlayIcon} alt="PlayIcon" />
-                  </button>
-
-                  <button className={style.like} onClick={onClick}>
-                    {like.isLike ? (
-                      <Image src={LikeButtonUnpressed} alt="LikeButton" />
-                    ) : (
-                      <Image src={LikeButtonPressed} alt="LikeButton" />
-                    )}
-                  </button>
-                  <button className={style.add}>
-                    <Image src={AddIcon} alt="AddIcon" />
-                  </button>
-                </div>
-              </div>
+            <div className={style.albumAndDate}>
+              <div className={style.album}>Album: {props.song.album}</div>
+              <div className={style.date}>{props.song.date}</div>
             </div>
           </div>
+          <div className={style.subbuttons}>
+            <button className={style.play}>
+              <Image src={PlayIcon} alt="PlayIcon" />
+            </button>
+
+            <button className={style.like} onClick={onClick}>
+              {like.isLike ? (
+                <Image src={LikeButtonUnpressed} alt="LikeButton" />
+              ) : (
+                <Image src={LikeButtonPressed} alt="LikeButton" />
+              )}
+            </button>
+            <button className={style.add}>
+              <Image src={AddIcon} alt="AddIcon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/spotifyApiTest/page.tsx
+++ app/spotifyApiTest/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import style from "./page.module.scss";
 
 const CLIENT_ID = "542aa9ff763e444fb06e2aa24ef157e3";
 const CLIENT_SECRET = "a24c3744fc184fa481e9b2da2d39607b";
@@ -18,9 +19,60 @@ export default function SpitifyApiCAll() {
       .then((result) => result.json())
       .then((data) => setAccessToken(data.access_token));
   }, []);
+
+  //Search
+  async function search() {
+    console.log(`Search for ${searchInput}`);
+
+    //get request using search to get the Artist ID
+    const searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    let artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+    console.log(`Artist Id is ${artistID}`);
+    //get request with Artist ID grab all the albums
+    let album = await fetch(
+      `https://api.spotify.com/v1/artists/${artistID}/albums`,
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    //display those albums
+  }
+
   return (
-    <div>
-      <div>Hi</div>
-    </div>
+    <>
+      <div className={style.container}>
+        <div className={style.search}>
+          <input
+            className={style.input}
+            type="text"
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button className={style.button} onClick={search}>
+            Search
+          </button>
+        </div>
+      </div>
+      <div className={style.container}></div>
+    </>
   );
 }
